fe2815b9882bafca3be9383bba433f17e39dabe2
Nitin Pandey <nitinpandey13@Nitins-MacBook-Air.local>
2026-01-23 11:57:51 +0530

    Subsequence of a string solution

---
+++ src/Recursion/Subsequence.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Subsequence {
+    public static void main(String[] args) {
+      String s="abc";
+      subsequence(s,"");
+      System.out.println(count);
+    }
+    static int count=0;
+    public static void subsequence(String ques, String ans){
+        if (ques.length()==0){
+            System.out.print(ans+ " ");
+            count++;
+            return;
+        }
+        char ch=ques.charAt(0);
+        subsequence(ques.substring(1),ans);
+        subsequence(ques.substring(1),ans+ch);
+
+    }
+}
