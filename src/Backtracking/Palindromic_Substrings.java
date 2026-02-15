b4e1a09508638167910da0f96a1e008bd6fc3ed8
Nitin Pandey <nitinpandey13@Nitins-MacBook-Air.local>
2026-02-16 00:24:40 +0530

    Palindromic Substrings count solution

---
+++ src/Backtracking/Palindromic_Substrings.java
@@ -0,0 +1,32 @@
+package Backtracking;
+
+public class Palindromic_Substrings {
+    public static void main(String[] args) {
+        String s = "aaa";
+        int ans = count_palindromic(s);
+        System.out.println(ans);
+    }
+
+    public static int count_palindromic(String s) {
+        int count = 0;
+        // odd
+        for (int axis = 0; axis < s.length(); axis++) {
+            for (int orbit = 0; axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
+                if (s.charAt(axis - orbit) != s.charAt(axis + orbit)) {
+                    break;
+                }
+                count++;
+            }
+        }
+        // even
+        for (double axis = 0.5; axis < s.length(); axis++) {
+            for (double orbit = 0.5; axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
+                if (s.charAt((int) (axis - orbit)) != s.charAt((int) (axis + orbit))) {
+                    break;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
