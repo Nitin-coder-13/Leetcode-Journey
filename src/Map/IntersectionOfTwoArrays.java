a4e3bf4b197060439b5652554ff7c412147cb3b8
Nitin-coder-13 <np5461736@gmail.com>
2026-05-02 12:16:19 +0530

    Intesection of Two Arrays-ii Solution

---
+++ src/Map/IntersectionOfTwoArrays.java
@@ -0,0 +1,37 @@
+package Map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class IntersectionOfTwoArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {4,9,5};
+        int[] arr2 = {9,4,9,8,4};
+        System.out.println(Arrays.toString(intersect_(arr1, arr2)));
+    }
+
+    public static int[] intersect_(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                list.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+
+    }
+}
